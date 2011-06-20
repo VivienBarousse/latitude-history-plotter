4adb2cb4199b16683e9b3a1080442077733d91e3
Vivien Barousse <barousse.vivien@gmail.com>
2011-06-20 14:37:10 +0100

    Project import

--- src/main/java/com/aperigeek/latitude/plotter/Main.java
+++ src/main/java/com/aperigeek/latitude/plotter/Main.java
@@ -16,6 +16,26 @@
  */
 package com.aperigeek.latitude.plotter;
 
+import com.aperigeek.geo.GeoLocation;
+import com.aperigeek.geo.image.plotter.BoundingBox;
+import com.aperigeek.geo.image.plotter.SequentialImagePlotter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.imageio.ImageIO;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 /**
  *
  * @author Vivien Barousse
@@ -23,6 +43,69 @@ package com.aperigeek.latitude.plotter;
 public class Main {
     
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar plotter.jar kml image");
+        }
+        
+        try {
+            File file = new File(args[0]);
+            KmlHandler handler = new KmlHandler();
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            parser.parse(file, handler);
+            
+            SequentialImagePlotter plotter = new SequentialImagePlotter(1600, 1200);
+            plotter.setBoundingBox(new BoundingBox(85, -85, 180, -180));
+            BufferedImage image = plotter.plot(handler.locations, null);
+            System.out.println(handler.locations);
+            
+            ImageIO.write(image, "png", new File(args[1]));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static class KmlHandler extends DefaultHandler {
+        
+        private static final Pattern coordinatesPattern = 
+                Pattern.compile("(-?[0-9\\.]+),(-?[0-9\\.]+),[0-9]+");
+        
+        private boolean inCoordinates = false;
+        
+        private List<GeoLocation> locations = new ArrayList<GeoLocation>();
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equals("coordinates")) {
+                inCoordinates = true;
+            }
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            String str = new String(ch, start, length);
+            if (inCoordinates) {
+                Matcher matcher = coordinatesPattern.matcher(str);
+                if (matcher.matches()) {
+                    locations.add(new GeoLocation(
+                            Double.parseDouble(matcher.group(2)), 
+                            Double.parseDouble(matcher.group(1))));
+                } else {
+                    System.out.println("Unexpected content " + str);
+                }
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (qName.equals("coordinates")) {
+                inCoordinates = false;
+            }
+        }
         
     }
     
